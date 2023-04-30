f4a0eadeea9ad3dd26152bc928e894f5a386fbc3
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2023-04-30 17:44:11 -0300

    Adding new enemy

--- Sprouts-Adventure/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Sprouts-Adventure/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -17,16 +17,91 @@ public class EnemyHealth : MonoBehaviour
     }
 
     public void Update()
-    {
+    {   
         health = enemy.health;
 
-        if (health == 7) GetComponent<SpriteRenderer>().sprite = sp6; 
-        else if (health == 6) GetComponent<SpriteRenderer>().sprite = sp5; 
-        else if (health == 5) GetComponent<SpriteRenderer>().sprite = sp4; 
-        else if (health == 4) GetComponent<SpriteRenderer>().sprite = sp3; 
-        else if (health == 3) GetComponent<SpriteRenderer>().sprite = sp2; 
-        else if (health == 2) GetComponent<SpriteRenderer>().sprite = sp1; 
-        else if (health == 1) GetComponent<SpriteRenderer>().sprite = sp1; 
-        else if (health == 0) Destroy(gameObject);
+        if (maxHealth == 10) 
+        {
+            if (health == 10) GetComponent<SpriteRenderer>().sprite = sp6; 
+            else if (health == 9) GetComponent<SpriteRenderer>().sprite = sp5; 
+            else if (health == 8) GetComponent<SpriteRenderer>().sprite = sp4; 
+            else if (health == 7) GetComponent<SpriteRenderer>().sprite = sp4; 
+            else if (health == 6) GetComponent<SpriteRenderer>().sprite = sp3; 
+            else if (health == 5) GetComponent<SpriteRenderer>().sprite = sp3; 
+            else if (health == 4) GetComponent<SpriteRenderer>().sprite = sp2; 
+            else if (health == 3) GetComponent<SpriteRenderer>().sprite = sp2; 
+            else if (health == 2) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 1) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 0) Destroy(gameObject);
+        }
+
+        if (maxHealth == 9) 
+        {
+            if (health == 9) GetComponent<SpriteRenderer>().sprite = sp6; 
+            else if (health == 8) GetComponent<SpriteRenderer>().sprite = sp5; 
+            else if (health == 7) GetComponent<SpriteRenderer>().sprite = sp4; 
+            else if (health == 6) GetComponent<SpriteRenderer>().sprite = sp3; 
+            else if (health == 5) GetComponent<SpriteRenderer>().sprite = sp3; 
+            else if (health == 4) GetComponent<SpriteRenderer>().sprite = sp2; 
+            else if (health == 3) GetComponent<SpriteRenderer>().sprite = sp2; 
+            else if (health == 2) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 1) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 0) Destroy(gameObject);
+        }
+
+        if (maxHealth == 8) 
+        {
+            if (health == 8) GetComponent<SpriteRenderer>().sprite = sp6; 
+            else if (health == 7) GetComponent<SpriteRenderer>().sprite = sp5; 
+            else if (health == 6) GetComponent<SpriteRenderer>().sprite = sp4; 
+            else if (health == 5) GetComponent<SpriteRenderer>().sprite = sp3; 
+            else if (health == 4) GetComponent<SpriteRenderer>().sprite = sp3; 
+            else if (health == 3) GetComponent<SpriteRenderer>().sprite = sp2; 
+            else if (health == 2) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 1) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 0) Destroy(gameObject);
+        }
+
+        if (maxHealth == 7) 
+        {
+            if (health == 7) GetComponent<SpriteRenderer>().sprite = sp6; 
+            else if (health == 6) GetComponent<SpriteRenderer>().sprite = sp5; 
+            else if (health == 5) GetComponent<SpriteRenderer>().sprite = sp4; 
+            else if (health == 4) GetComponent<SpriteRenderer>().sprite = sp3; 
+            else if (health == 3) GetComponent<SpriteRenderer>().sprite = sp2; 
+            else if (health == 2) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 1) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 0) Destroy(gameObject);
+        }
+
+        if (maxHealth == 6) 
+        {
+            if (health == 6) GetComponent<SpriteRenderer>().sprite = sp6; 
+            else if (health == 5) GetComponent<SpriteRenderer>().sprite = sp4; 
+            else if (health == 4) GetComponent<SpriteRenderer>().sprite = sp3; 
+            else if (health == 3) GetComponent<SpriteRenderer>().sprite = sp2; 
+            else if (health == 2) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 1) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 0) Destroy(gameObject);
+        }
+
+        if (maxHealth == 5) 
+        {
+            if (health == 5) GetComponent<SpriteRenderer>().sprite = sp6; 
+            else if (health == 4) GetComponent<SpriteRenderer>().sprite = sp5; 
+            else if (health == 3) GetComponent<SpriteRenderer>().sprite = sp4; 
+            else if (health == 2) GetComponent<SpriteRenderer>().sprite = sp2; 
+            else if (health == 1) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 0) Destroy(gameObject);
+        }
+
+        if (maxHealth == 4) 
+        {
+            if (health == 4) GetComponent<SpriteRenderer>().sprite = sp6; 
+            else if (health == 3) GetComponent<SpriteRenderer>().sprite = sp4; 
+            else if (health == 2) GetComponent<SpriteRenderer>().sprite = sp2; 
+            else if (health == 1) GetComponent<SpriteRenderer>().sprite = sp1; 
+            else if (health == 0) Destroy(gameObject);
+        }
     }
 }
