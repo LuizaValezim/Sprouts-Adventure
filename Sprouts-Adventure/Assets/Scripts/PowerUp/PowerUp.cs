b340095e69791feb4f235d736dc9aa046ee8cf59
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2023-05-01 17:52:42 -0300

    Adding icons

--- Sprouts-Adventure/Assets/Scripts/PowerUp/PowerUp.cs
+++ Sprouts-Adventure/Assets/Scripts/PowerUp/PowerUp.cs
@@ -6,11 +6,14 @@ public class PowerUp : MonoBehaviour
 {
     public PowerUpEffect PowerUpEffect;
     public GameObject item;
+    private AudioSource source;
+    public AudioClip clip;
 
     public void UsePowerUp()
     {
         PowerUpEffect.Apply(GameObject.FindGameObjectWithTag("Sprout"));
-        Debug.Log("apple");
         Destroy(item);
+        source = GameObject.FindGameObjectWithTag("Sprout").GetComponent<AudioSource>();
+        source.PlayOneShot(clip);
     }
 }
