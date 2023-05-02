368de0db8211334b1e1f230b1e347931a14f4824
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2023-05-01 23:29:00 -0300

    Tutorial

--- Sprouts-Adventure/Assets/Scripts/Cow/CowHealth.cs
+++ Sprouts-Adventure/Assets/Scripts/Cow/CowHealth.cs
@@ -21,13 +21,13 @@ public class CowHealth : MonoBehaviour
     {   
         health = cow.health;
 
-        if (maxHealth == 30) 
+        if (maxHealth == 20) 
         {
-            if (health == 30) { GetComponent<SpriteRenderer>().sprite = sp6; img.sprite = sp6; } 
-            else if (health == 25) { GetComponent<SpriteRenderer>().sprite = sp5; img.sprite = sp5; } 
-            else if (health == 20) { GetComponent<SpriteRenderer>().sprite = sp4; img.sprite = sp4; } 
-            else if (health == 15) { GetComponent<SpriteRenderer>().sprite = sp3; img.sprite = sp3; } 
-            else if (health == 10) { GetComponent<SpriteRenderer>().sprite = sp2; img.sprite = sp2; } 
+            if (health == 20) { GetComponent<SpriteRenderer>().sprite = sp6; img.sprite = sp6; } 
+            else if (health == 17) { GetComponent<SpriteRenderer>().sprite = sp5; img.sprite = sp5; } 
+            else if (health == 15) { GetComponent<SpriteRenderer>().sprite = sp4; img.sprite = sp4; } 
+            else if (health == 10) { GetComponent<SpriteRenderer>().sprite = sp3; img.sprite = sp3; } 
+            else if (health == 7) { GetComponent<SpriteRenderer>().sprite = sp2; img.sprite = sp2; } 
             else if (health == 5) { GetComponent<SpriteRenderer>().sprite = sp1; img.sprite = sp1; } 
             else if (health == 0) Destroy(gameObject);
         }
